f82a1f316e641ba27749b131a83a051803fa71c4
apurbojamananis <apurbojaman.anis@gmail.com>
2023-06-21 23:51:24 +0600

    update server link

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosSecure = axios.create({
   // baseURL: "http://localhost:5000",
-  baseURL: "https://sports-summer-camp-server-two.vercel.app/",
+  baseURL: "https://sports-summer-camp-server-apurbojamananis.vercel.app/",
 });
 
 const useAxiosSecure = () => {
